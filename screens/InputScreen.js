5e601d738285a4b03097f82ea4be2ee627f03782
regiss05 <regiss.steven@gmail.com>
2025-01-18 21:57:16 +0200

    configuration

---
+++ screens/InputScreen.js
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+const InputScreen = ({ navigation }) => {
+  const [cardNumber, setCardNumber] = useState('');
+  const [packageName, setPackageName] = useState('');
+  const [duration, setDuration] = useState('');
+  const [transactionDate, setTransactionDate] = useState('');
+  const [transactionTime, setTransactionTime] = useState('');
+  const [prefix, setPrefix] = useState('PW'); // State for dropdown selection
+
+  const getCurrentDate = () => new Date().toISOString().split('T')[0];
+  const getCurrentTime = () => new Date().toLocaleTimeString();
+
+  useEffect(() => {
+    setTransactionDate(getCurrentDate());
+    setTransactionTime(getCurrentTime());
+
+    // Update time every 30 seconds
+    const timeInterval = setInterval(() => {
+      setTransactionTime(getCurrentTime());
+    }, 30000); // 30000 ms = 30 seconds
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(timeInterval);
+  }, []);
+
+  const handleSubmit = () => {
+    navigation.navigate('ResultScreen', {
+      cardNumber,
+      packageName,
+      duration,
+      prefix,
+      transactionDate,
+      transactionTime,
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('./../assets/agesta.png')}
+      />
+      <Text style={styles.title}>AFRICAN GENIUS STAFF / AGESTA</Text>
+      <Text style={styles.subtitle}>CANAL + PAYMENT</Text>
+
+      {/* Card Number Input with Dropdown */}
+      <View style={styles.row}>
+        <Picker
+          selectedValue={prefix}
+          style={styles.picker}
+          onValueChange={(itemValue) => setPrefix(itemValue)}
+        >
+          <Picker.Item label="PWY" value="PWY" />
+          <Picker.Item label="PFL" value="PFL" />
+        </Picker>
+        <TextInput
+          style={{ 
+            borderColor: '#fff',
+            borderWidth: 1,
+            borderRadius: 5,
+            color: 'white',
+            padding: 10, 
+            width: '70%',
+          }}
+          placeholder="Card Number"
+          placeholderTextColor="gray"
+          keyboardType="numeric"
+          value={cardNumber}
+          onChangeText={setCardNumber}
+        />
+      </View>
+
+      {/* Package Name Picker */}
+      <Picker
+        selectedValue={packageName}
+        style={styles.input}
+        onValueChange={(itemValue) => setPackageName(itemValue)}
+      >
+        <Picker.Item label="Select a package" value="" />
+        <Picker.Item label="ACCESS" value="ACCESS" />
+        <Picker.Item label="EVASION" value="EVASION" />
+        <Picker.Item label="ACCESS+" value="ACCESS+" />
+        <Picker.Item label="EVASION+" value="EVASION+" />
+        <Picker.Item label="TOUT CANAL+" value="TOUT CANAL+" />
+        <Picker.Item label="ENGLISH PLUS" value="ENGLISH PLUS" />
+        <Picker.Item label="EVASION & ENGLISH+" value="EVASION & ENGLISH+" />
+        <Picker.Item label="ACCESS+ & ENGLISH+" value="ACCESS+ & ENGLISH+" />
+        <Picker.Item label="EVASION+ & ENGLISH+" value="EVASION+ & ENGLISH+" />
+        <Picker.Item label="ENGLISH+ & TOUT CANAL+" value="ENGLISH+ & TOUT CANAL+" />
+        <Picker.Item label="ACCESS -> EVASION" value="ACCESS -> EVASION" />
+        <Picker.Item label="ACCESS -> ACCESS+" value="ACCESS -> ACCESS+" />
+        <Picker.Item label="ACCESS -> EVASION+" value="ACCESS -> EVASION+" />
+        <Picker.Item label="ACCESS -> TOUT CANAL+" value="ACCESS -> TOUT CANAL+" />
+        <Picker.Item label="EVASION -> ACCESS+" value="EVASION -> ACCESS+" />
+        <Picker.Item label="EVASION -> EVASION+" value="EVASION -> EVASION+" />
+        <Picker.Item label="EVASION -> TOUT CANAL+" value="EVASION -> TOUT CANAL+" />
+        <Picker.Item label="ACCESS+ -> EVASION+" value="ACCESS+ -> EVASION+" />
+        <Picker.Item label="ACCESS+ -> TOUT CANAL+" value="ACCESS+ -> TOUT CANAL+" />
+        <Picker.Item label="EVASION+ -> TOUT CANAL+" value="EVASION+ -> TOUT CANAL+" />
+      </Picker>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Duration/Month"
+        placeholderTextColor="gray"
+        keyboardType="numeric"
+        value={duration}
+        onChangeText={setDuration}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Date of Transaction (YYYY-MM-DD)"
+        value={transactionDate}
+        onChangeText={setTransactionDate}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Time of Transaction (HH:MM:SS)"
+        value={transactionTime}
+        onChangeText={setTransactionTime}
+      />
+
+      <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#363636',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    marginBottom: 20,
+    marginTop: 20,
+    resizeMode: 'contain',
+  },
+  title: {
+    alignSelf: 'center',
+    marginBottom: 5,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  subtitle: {
+    alignSelf: 'center',
+    marginBottom: 15,
+    color: 'white',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  picker: {
+    flex: 1,
+    borderColor: '#fff',
+    borderWidth: 1,
+    marginRight: 3,
+    color: 'white',
+  },
+  input: {
+    borderColor: '#fff',
+    borderWidth: 1,
+    borderRadius: 5,
+    color: 'white',
+    padding: 10,
+    marginBottom: 5,
+  },
+  submitButton: {
+    backgroundColor: '#f98935',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default InputScreen;
---
+++ screens/ResultScreen.js
@@ -0,0 +1,105 @@
+import React, { useRef } from 'react';
+import { View, Text, StyleSheet, Button, Share, Image, Alert } from 'react-native';
+import ViewShot from 'react-native-view-shot';
+
+const ResultScreen = ({ route, navigation }) => {
+  const { cardNumber, packageName, duration, prefix, transactionDate, transactionTime } = route.params;
+  const viewShotRef = useRef(); // Ref for capturing the view
+
+  const shareBillDetails = async () => {
+    try {
+      const uri = await viewShotRef.current.capture(); // Capture the screenshot
+
+      await Share.share({
+        // message: `Bill Details:\nCard Number: ${cardNumber}\nPackage Name: ${packageName}\nDuration/Month: ${duration} ${prefix}\nDate of Transaction: ${transactionDate}`,
+        url: uri, // Share the captured screenshot
+      });
+
+    } catch (error) {
+      Alert.alert('Error', 'Failed to share bill details.');
+      console.error('Error sharing bill details:', error);
+    }
+  };
+
+  return (
+    <View style={{ backgroundColor: '#363636', flex: 1 }}>
+      <ViewShot
+        ref={viewShotRef}
+        options={{ format: 'jpg', quality: 0.9 }}
+        style={styles.container}
+      >
+        <View style={{ backgroundColor: '#f98935', padding: 5 }}>
+          <Text style={styles.header}>AFRICAN GENIUS STAFF / AGESTA</Text>
+          <Text style={styles.header}>CANAL PAYMENT - {prefix}</Text>
+        </View>
+
+        <View style={[styles.rowContainer, { backgroundColor: '#363636', padding: 20 }]}>
+          <View style={styles.detailsContainer}>
+            <Text style={styles.label}>Card No: {cardNumber}</Text>
+            <Text style={styles.label}>Package: {packageName}</Text>
+            <Text style={styles.label}>Duration/Month: {duration}</Text>
+            <Text style={styles.label}>Date: {transactionDate} {transactionTime}</Text>
+          </View>
+          <Image
+            style={styles.image}
+            source={require('./../assets/agesta.png')}
+          />
+        </View>
+        <View style={{ backgroundColor: '#6d18bd', padding: 5, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 5 }}>
+          <Image
+            style={styles.wlogo}
+            source={require('./../assets/whatsapp.png')}
+          />
+          <Text style={styles.labelnum}>+257 61 85 40 88</Text>
+        </View>
+        {/* <Button title="Share Bill as Image" onPress={shareBillDetails} /> */}
+      </ViewShot>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f98935',
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  detailsContainer: {
+    flex: 1,
+  },
+  header: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  label: {
+    fontSize: 15,
+    marginBottom: 5,
+    color: 'white',
+  },
+  labelnum: {
+    fontSize: 15,
+    marginBottom: 5,
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    alignSelf: 'center',
+    marginTop: -10,
+    resizeMode: 'contain',
+  },
+  wlogo: {
+    width: 25,
+    height: 25,
+    // alignSelf: 'center',
+    // marginTop: -10,
+    // resizeMode: 'contain',
+  },
+});
+
+export default ResultScreen;
